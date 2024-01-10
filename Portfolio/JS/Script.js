66922143c9b57b520d1ddc3e3ac0d0d7ad1c5767
BrightWurd <125743956+BrightWurd@users.noreply.github.com>
2024-01-10 01:49:10 +0100

    Add files via upload

---
+++ Portfolio/JS/Script.js
@@ -0,0 +1,5 @@
+// After the page loads the opacity transition will take place
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".main").classList.add("show");
+});
